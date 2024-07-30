b2441f2df779fba49aaffee94bb071375e066873
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2024-07-30 19:53:30 +0000

    install node and setup

---
+++ basics_01/redux_basics01.js
@@ -0,0 +1,11 @@
+// function addtask(task){
+//   return {
+//     type:'ADD_TASK',
+    
+//     payload:{
+//       id:1,
+//       task:task,
+//       status:'completed'
+//     }
+//   }
+console.log("hello");
